0d68f2cad79406a0dc0dbb88bb54a02c3d1e6521
Timothe Bissonnette <timothe5@hotmail.com>
2024-10-07 09:23:59 -0400

    testing with body protrait

--- src/Components/MyExperiences/MyExperiences.js
+++ src/Components/MyExperiences/MyExperiences.js
@@ -9,8 +9,7 @@ const MyExperiences = ({ handlePageChange }) => {
         <h1>My experiences</h1>
         <p>My real job experiences are pretty simple, I have been working in customer service, more precisely in a grocery store for the past 9 years.
             Although, on paper, there isn't any link with development, you would be suprised the amount of useful <span className={styles.span} >qualities</span> I have developped working there.
-            I think <span className={styles.span}>one that applies well to web development is the ability to understand the problem before trying to solve it</span>. For a hundred customers 
-            with the same problem there will be roughly 100 different ways the problem will be presented to you.
+            I think <span className={styles.span}>one that applies well to web development is the ability to understand the problem before trying to solve it</span>. 
         </p>
 
         <p>Aside from that, I am always looking to work on some new challenging projects. My most serious one is a website I am building for a company
